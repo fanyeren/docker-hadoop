ab992c5ad2fc619d1dca25593ab03a10207990b3
xiahou feng <xiahoufeng@yahoo.com>
2014-10-13 19:49:41 +0800

    xiahoufeng

--- hadoop-dn/config/start-hadoop.sh
+++ hadoop-dn/config/start-hadoop.sh
@@ -2,9 +2,7 @@
 
 sleep 4
 if [ "$NODE_TYPE" = "m" ]; then
-   su - work -c "$HADOOP_INSTALL/sbin/hadoop-daemon.sh start namenode"
    su - work -c "$HADOOP_INSTALL/sbin/hadoop-daemon.sh start datanode"
-   #su work -c "$HADOOP_INSTALL/sbin/start-yarn.sh"
 fi
 
 /etc/serf/start-serf-agent.sh
--- start-cluster.sh
+++ start-cluster.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
-docker run -d -t --dns 127.0.0.1 -e NODE_TYPE=s -P --name slave1 -h slave1.mycorp.kom alvinhenrick/hadoop-dn
+docker run -d -t --dns 127.0.0.1 -p 33001:22 -e NODE_TYPE=s -P --name slave1 -h dn1.xiahoufeng.com xiahoufeng/hadoop-dn
+docker run -d -t --dns 127.0.0.1 -p 33302:22 -e NODE_TYPE=s -P --name slave2 -h dn2.xiahoufeng.com xiahoufeng/hadoop-dn
 FIRST_IP=$(docker inspect --format="{{.NetworkSettings.IPAddress}}" slave1)
-docker run -i -t --dns 127.0.0.1 -e NODE_TYPE=m -e JOIN_IP=$FIRST_IP -P --name master -h master.mycorp.kom alvinhenrick/hadoop-nn-dn
+SECOND_IP=$(docker inspect --format="{{.NetworkSettings.IPAddress}}" slave2)
+docker run -i -t --dns 127.0.0.1 -p 30001:22 -e NODE_TYPE=m -e JOIN_IP=$FIRST_IP,$SECOND_IP -P --name master -h nn0.xiahoufeng.com xiahoufeng/hadoop-nn
 
