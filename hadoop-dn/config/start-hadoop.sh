7ead6048e6c84d1abee83a2a150fb3ae82eaef0e
xiahou feng <xiahoufeng@yahoo.com>
2014-10-13 20:29:01 +0800

    xiahoufeng

--- hadoop-dn/config/start-hadoop.sh
+++ hadoop-dn/config/start-hadoop.sh
@@ -5,4 +5,7 @@ if [ "$NODE_TYPE" = "m" ]; then
    su - work -c "$HADOOP_INSTALL/sbin/hadoop-daemon.sh start datanode"
 fi
 
+service ssh start
+service rsyslog start
+
 /etc/serf/start-serf-agent.sh
--- hadoop-nn/config/start-hadoop.sh
+++ hadoop-nn/config/start-hadoop.sh
@@ -7,4 +7,7 @@ if [ "$NODE_TYPE" = "m" ]; then
    #su work -c "$HADOOP_INSTALL/sbin/start-yarn.sh"
 fi
 
+service ssh start
+service rsyslog start
+
 /etc/serf/start-serf-agent.sh
