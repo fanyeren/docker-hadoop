d6cafe07c2a47027ccb82db4cfa7313dac43c408
xiahou feng <xiahoufeng@yahoo.com>
2014-10-13 23:32:20 +0800

    xiahoufeng

--- start-cluster.sh
+++ start-cluster.sh
@@ -1,10 +1,15 @@
 #!/bin/bash
 
 docker run -d --dns 127.0.0.1 -p 33001:22 -e NODE_TYPE=s -P --name slave1 -h dn1.xiahoufeng.com xiahoufeng/hadoop-dn
+sleep 2
 FIRST_IP=$(docker inspect --format="{{.NetworkSettings.IPAddress}}" slave1)
 
 docker run -d --dns 127.0.0.1 -p 33002:22 -e NODE_TYPE=s -P --name slave2 -h dn2.xiahoufeng.com -e JOIN_IP=$FIRST_IP xiahoufeng/hadoop-dn
-docker run -d --dns 127.0.0.1 -p 33003:22 -e NODE_TYPE=s -P --name slave3 -h dn2.xiahoufeng.com -e JOIN_IP=$FIRST_IP xiahoufeng/hadoop-dn
-docker run -d --dns 127.0.0.1 -p 33004:22 -e NODE_TYPE=s -P --name slave4 -h dn2.xiahoufeng.com -e JOIN_IP=$FIRST_IP xiahoufeng/hadoop-dn
-docker run -d --dns 127.0.0.1 -p 33005:22 -e NODE_TYPE=s -P --name slave5 -h dn2.xiahoufeng.com -e JOIN_IP=$FIRST_IP xiahoufeng/hadoop-dn
+sleep 2
+docker run -d --dns 127.0.0.1 -p 33003:22 -e NODE_TYPE=s -P --name slave3 -h dn3.xiahoufeng.com -e JOIN_IP=$FIRST_IP xiahoufeng/hadoop-dn
+sleep 2
+docker run -d --dns 127.0.0.1 -p 33004:22 -e NODE_TYPE=s -P --name slave4 -h dn4.xiahoufeng.com -e JOIN_IP=$FIRST_IP xiahoufeng/hadoop-dn
+sleep 2
+docker run -d --dns 127.0.0.1 -p 33005:22 -e NODE_TYPE=s -P --name slave5 -h dn5.xiahoufeng.com -e JOIN_IP=$FIRST_IP xiahoufeng/hadoop-dn
+sleep 2
 docker run -d --dns 127.0.0.1 -p 30001:22 -e NODE_TYPE=m -P --name master -h nn0.xiahoufeng.com -e JOIN_IP=$FIRST_IP xiahoufeng/hadoop-nn
